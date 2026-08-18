995c00b25eea1f80bda1b344f471fbf3365f904a
SanketBarde <sanketbarde7322@gmail.com>
2026-08-18 15:33:05 +0530

    keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEDD2EjbfjhIrJzTaUQxlklVpNJgpSgbY",
-  authDomain: "theplottwist.firebaseapp.com",
-  projectId: "theplottwist",
-  storageBucket: "theplottwist.firebasestorage.app",
-  messagingSenderId: "1017380294785",
-  appId: "1:1017380294785:web:aa2b769c8b90d387f8fd80",
-  measurementId: "G-1R2MZ8BHN1"
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measured_id: VITE_MEASURED_ID
 };
 
 // Initialize Firebase
